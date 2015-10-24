26b93bb596c0c6991c96110237b2f879bbb7d3a8
Joseph Satterfield <jjs228@case.edu>
2015-10-24 14:15:28 -0400

    Basic views and templates for Board listings and thread listings
    Signed-off-by: Joseph Satterfield <jjs228@case.edu>

--- whiteboard/wbMessageBoard/views.py
+++ whiteboard/wbMessageBoard/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models import DiscussionBoard, Thread
 
 # Create your views here.
+
+def index(request):
+    board_list = DiscussionBoard.objects.all()
+
+    template = loader.get_template('wbMessageBoard/index.html')
+    context = RequestContext(request, {
+        'board_list': board_list,
+    })
+    return HttpResponse(template.render(context))
+
+def boards(request, board_id):
+    #current_board = DiscussionBoard.objects.get(pk=board_id)
+    thread_list = Thread.objects.filter(board = board_id).order_by('-time_of_creation')
+
+    template = loader.get_template('wbMessageBoard/board.html')
+    context = RequestContext(request, {
+        'thread_list': thread_list,
+        'board': board_id,
+    })
+    return HttpResponse(template.render(context))
