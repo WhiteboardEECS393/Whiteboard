d2f039e2a2a31839b313b31a4dc0ad787d37629a
mje57 <mje57@case.edu>
2015-10-26 02:10:30 -0400

    Added Classpage template and css file

--- whiteboard/Profiles/views.py
+++ whiteboard/Profiles/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
 from django.template import RequestContext, loader
 from .models import BasicUser, StudentUser, ProfessorUser
 
@@ -9,9 +8,9 @@ def index(request):
     professor_profile_list = ProfessorUser.objects.all()
 
     # template = loader.get_template('Profiles/index.html')
-    template = loader.get_template('Profiles/classpage.html')
+    template = 'Profiles/index.html'
     context = RequestContext(request, {
         'student_profile_list': student_profile_list,
         'professor_profile_list': professor_profile_list,
     })
-    return HttpResponse(template.render(context))
+    return render_to_response(template, locals(), context)
