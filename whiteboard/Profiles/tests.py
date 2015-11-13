cf623cd07d7e1db45489d66d87c87af49d918891
Danny2036 <dnm30@case.edu>
2015-11-12 23:09:57 -0500

    Fixed all bugs in Profiles test and assertions are corrected

--- whiteboard/Profiles/tests.py
+++ whiteboard/Profiles/tests.py
@@ -16,39 +16,44 @@ def createMinor(self):
     newMinor = Minor(minor = "Game Design")
     return newMinor
 def createStudentUser(self):
-    user = User.objects.create_user(
-            username='jacob', email='jacob@…', password='top_secret')
+    newStudent = StudentUser(user = User.objects.create_user(username='Daniel',
+                                                         email='abc@case.edu',
+                                                         password='password'))
+
+    return  newStudent
 def createDepartment(self):
-    departement_code = "EECS"
-    departement_name = "Electrical Engineering and Computer Science"
-    departement_info = "Stuff about the department"
+    newdepartment = Department(department_code = "EECS",
+                               department_name = "Electrical Engineering and Computer Science",
+                               department_info = "Stuff about the department")
+    return newdepartment
 def createProfessor(self):
-    first_name = "Tekin"
-    last_name = "Osoy....glu"
-    email_id = "abc@case.edu"
-    bio = "Something or the other"
-    office_Location = "Where the sun don't shine"
+    newProfessor = Professor(first_name = "Tekin",
+                             last_name = "Osoy....glu",
+                             email_id = "abc@case.edu",
+                             bio = "Something or the other",
+                             office_location = "Where the sun don't shine")
+    return newProfessor
 
 class ProfilesMethodTests(TestCase):
     def test_Major_str(self):
         testMajor = createMajor(self)
-        self.assertEquals(testMajor.major, testMajor.__str__(self))
+        self.assertEquals(testMajor.major, testMajor.__str__())
 
     def test_Minor_str(self):
         test_Minor = createMinor(self)
-        self.assertEquals(test_Minor.minor, test_Minor.__str__(self))
+        self.assertEquals(test_Minor.minor, test_Minor.__str__())
 
     def test_Student_str(self):
         testStudentUser = createStudentUser(self)
-        self.assertEquals(testStudentUser.first_name + " " + testStudentUser.last_name, testStudentUser.__str__(self))
+        self.assertEquals(testStudentUser.first_name + " " + testStudentUser.last_name, testStudentUser.__str__())
 
     def test_Department_str(self):
         testDepartment = createDepartment(self)
-        self.assertEquals(testDepartment.department_name, testDepartment.__str__(self))
+        self.assertEquals(testDepartment.department_name, testDepartment.__str__())
 
     def test_Professor_str(self):
         testProfessor = createProfessor(self)
-        self.assertEquals(testProfessor.first_name + " " + testProfessor.last_name, testProfessor.__str__(self))
+        self.assertEquals(testProfessor.first_name + " " + testProfessor.last_name, testProfessor.__str__())
 '''
 #Needed for testing BasicUser
 #How do you make a user?
