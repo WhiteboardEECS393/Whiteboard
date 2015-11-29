8597fe53824e2d20215c01773e8b29632ab32acb
Joseph Satterfield <jjs228@case.edu>
2015-11-29 09:22:17 -0500

    Added migration files for here
    Signed-off-by: Joseph Satterfield <jjs228@case.edu>

---
+++ whiteboard/Profiles/migrations/0001_initial.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('class_overviews', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Department',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('department_code', models.CharField(max_length=4)),
+                ('department_name', models.CharField(max_length=100)),
+                ('department_info', models.CharField(max_length=500)),
+            ],
+            options={
+                'ordering': ['department_name'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Major',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('major', models.CharField(max_length=50)),
+                ('required_classes', models.ManyToManyField(to='class_overviews.Course', blank=True)),
+            ],
+            options={
+                'ordering': ['major'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Minor',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('minor', models.CharField(max_length=50)),
+                ('required_classes', models.ManyToManyField(to='class_overviews.Course', blank=True)),
+            ],
+            options={
+                'ordering': ['minor'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Professor',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('first_name', models.CharField(max_length=100)),
+                ('last_name', models.CharField(max_length=100)),
+                ('email_id', models.EmailField(max_length=254)),
+                ('bio', models.CharField(max_length=500)),
+                ('office_location', models.CharField(max_length=100)),
+                ('photo', models.CharField(default='none', max_length=200)),
+                ('classes', models.ManyToManyField(to='class_overviews.Section', blank=True)),
+                ('current_department', models.ForeignKey(to='Profiles.Department', blank=True, null=True)),
+            ],
+            options={
+                'ordering': ['last_name', 'first_name'],
+            },
+        ),
+        migrations.CreateModel(
+            name='StudentUser',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('first_name', models.CharField(default='First', max_length=100)),
+                ('last_name', models.CharField(default='Last', max_length=100)),
+                ('email_id', models.EmailField(default='abc123@case.edu', max_length=254)),
+                ('bio', models.CharField(default='none', max_length=500)),
+                ('photo', models.CharField(default='none', max_length=200)),
+                ('grad_year', models.IntegerField(default=2016)),
+                ('majors', models.ManyToManyField(to='Profiles.Major', blank=True)),
+                ('minors', models.ManyToManyField(to='Profiles.Minor', blank=True)),
+                ('student_classes', models.ManyToManyField(to='class_overviews.Section', blank=True, related_name='studentuser_student')),
+                ('ta_classes', models.ManyToManyField(to='class_overviews.Section', blank=True, related_name='studentuser_ta')),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['last_name', 'first_name'],
+            },
+        ),
+        migrations.AddField(
+            model_name='department',
+            name='department_head',
+            field=models.ForeignKey(to='Profiles.Professor', blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name='department',
+            name='majors',
+            field=models.ManyToManyField(to='Profiles.Major', blank=True),
+        ),
+        migrations.AddField(
+            model_name='department',
+            name='minors',
+            field=models.ManyToManyField(to='Profiles.Minor', blank=True),
+        ),
+    ]
---
+++ whiteboard/class_overviews/migrations/0001_initial.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Course',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('department', models.CharField(max_length=4)),
+                ('course_number', models.IntegerField(default=999)),
+                ('course_name', models.CharField(max_length=200)),
+                ('description', models.CharField(max_length=300, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Document',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('description', models.CharField(max_length=300, blank=True)),
+                ('file', models.FileField(upload_to='class_overviews/static/documents')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Section',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('location', models.CharField(max_length=50)),
+                ('start_time', models.TimeField(blank=True)),
+                ('end_time', models.TimeField(blank=True)),
+                ('days_of_week', models.CharField(max_length=7)),
+                ('section_number', models.IntegerField(default=0)),
+                ('course', models.ForeignKey(to='class_overviews.Course')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Semester',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('season', models.CharField(default='Fall', max_length=10)),
+                ('year', models.IntegerField(default=2015)),
+                ('startDate', models.DateField()),
+                ('endDate', models.DateField()),
+            ],
+        ),
+        migrations.AddField(
+            model_name='section',
+            name='semester',
+            field=models.ForeignKey(to='class_overviews.Semester'),
+        ),
+        migrations.AddField(
+            model_name='document',
+            name='course_section',
+            field=models.ForeignKey(to='class_overviews.Section'),
+        ),
+    ]
